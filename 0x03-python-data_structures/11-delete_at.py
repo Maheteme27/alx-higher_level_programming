6910e05b85bbc5c286ce8393c5a56e6f8f4d50bc
Maheteme27 <maheteme380@gmail.com>
2023-04-02 09:14:21 -0700

    updates

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-# 11-delete_at.py
 def delete_at(my_list=[], idx=0):
-    """Delete an item at a specific position in a list."""
-
-mati(马哈）, [3/31/2023 10:04 AM]
-    if idx >= 0 and idx < len(my_list):
-        del my_list[idx]
-    return (my_list)
+    if idx >= 0 and idx < len(my_list):
+        del my_list[idx]
+    return(my_list)
--- 0x03-python-data_structures/README.md
+++ 0x03-python-data_structures/README.md
@@ -1,7 +1,1 @@
-README.md
-# Python - Data Structures: Lists, Tuples
-In this project, I learned about how sequence data types work in
-Python - specifically, lists and tuples.
-## Tests :heavy_check_mark:
-* [tests](./tests): Folder of test files. Provided by ALX School.
-## Function Prototypes :floppy_disk:
+0x03. Python - Data Structures: Lists, Tuples
